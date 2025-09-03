e3eb340aa25c3c5163b7d920938c604cf59b6649
Thomas Rokicki <thomas.rokicki@centralesupelec.fr>
2025-09-03 18:49:18 +0200

    exercise 1

---
+++ exercice1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+
+// Remainder: to print an integer, you can use
+// print("%d\n", i);
+int main(){
+    // Declare two ints
+    ...
+
+    // Add them and store them in a new variable
+    ...
+
+    // Compute the euclidean division of the two integers
+
+    // Store the 3rd bit of the first integer in a new variable
+
+    return 0;
+
+}
